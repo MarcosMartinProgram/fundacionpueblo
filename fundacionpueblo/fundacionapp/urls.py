1de19a56fe4c19804ea80ab7ae407849981bae98
marcos <martinmarcospablo@gmail.com>
2022-12-04 23:04:19 -0300

    eventos termindo

--- fundacionpueblo/fundacionapp/urls.py
+++ fundacionpueblo/fundacionapp/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 
 from fundacionapp import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     
     path('', views.home, name='Home'),
     path('somos', views.somos, name='Quienes Somos'),
     path('blog', views.blog, name='Blog'),
-    path('eventos', views.eventos, name='Eventos'),
     path('contacto', views.contacto, name='Contacto'),
     
     
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
